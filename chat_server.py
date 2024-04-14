15a14707cb244400ccb0b10ae1417266fc3809b0
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-04-13 22:00:54 -0700

    optional args

--- chat_server.py
+++ chat_server.py
@@ -1,43 +1,42 @@
-# Chat server
- 
-import sys
 import socket
 import select
+import argparse
 
-HOST = '' 
+# optional arguments hooraay 🎉
+parser = argparse.ArgumentParser(description='Chat server that listens on a specified port and allows multiple clients to connect and chat with each other.')
+parser.add_argument('--port', type=int, default=9009, help='Port number to listen on. Default is 9009.')
+parser.add_argument('--host', type=str, default='localhost', help='Host name to listen on. Default is localhost.')
+parser.add_argument('--buffer', type=int, default=4096, help='Buffer size for receiving messages. Default is 4096.')
+
+args = parser.parse_args()
+
+HOST = args.host
+PORT = args.port
+RECV_BUFFER = args.buffer
 SOCKET_LIST = []
-RECV_BUFFER = 4096 
-PORT = 9009
-
 
 def chat_server():
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Using a stream socket because data integrity is important
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # IPv4, TCP
     server_socket.bind((HOST, PORT))
     server_socket.listen(10)
 
-    # add server socket object to the list of readable connections
     SOCKET_LIST.append(server_socket)
  
     print ("Chat server started on port " + str(PORT))
 
-    while 1:
-
-        # get the list sockets which are ready to be read through select
-        # 4th arg, time_out  = 0 : poll and never block
+    while True:
+        # Select lets us monitor sockets, we only use the read list
         ready_to_read, ready_to_write, in_error = select.select(SOCKET_LIST, [], [], 0)
 
         for sock in ready_to_read:
-            # a new connection request received
-            if sock == server_socket:
+            if sock == server_socket:   # if there's a client connection
                 sockfd, addr = server_socket.accept()
                 SOCKET_LIST.append(sockfd)
                 print ("Client (%s, %s) connected" % addr)
 
                 broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
-
-            # a message from a client, not a new connection
             else:
                 # process data recieved from client,
                 try:
